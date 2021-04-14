75d396aad476d5b5e40d9a7f5819d708fdc341c5
Vlad Coteanu <vlad.coteanu@uipath.com>
2021-04-14 16:54:59 +0300

    Implemented Workflow - Flowchart parser

---
+++ XMLParsing/XMLParsing/XMLParsing/Common/Node.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace XMLParsing.Common
+{
+    class Node
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public List<Transition> TransitionList { get; set; }
+
+        public Node() 
+        {
+            TransitionList = new List<Transition>();
+        }
+    }
+}
---
+++ XMLParsing/XMLParsing/XMLParsing/Services/WorkflowParser.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Activities;
+using System.Activities.XamlIntegration;
+using System.IO;
+using System.Xaml;
+using System.Xml;
+using UiPath.Core.Activities;
+using XMLParsing.Common;
+
+namespace XMLParsing.Services
+{
+    class WorkflowParser
+    {
+
+        private static readonly Lazy<WorkflowParser> LazyInstance = new Lazy<WorkflowParser>(() => new WorkflowParser());
+
+        private WorkflowParser()
+        {
+        }
+
+        public static WorkflowParser Instance
+        {
+            get
+            {
+                return LazyInstance.Value;
+            }
+        }
+
+        public Workflow ParseWorkflow(string path)
+        {
+            string readText = File.ReadAllText(path);
+
+
+            // here
+            //xmlns.AddNamespace("ui", "http://schemas.uipath.com/workflow/activities");
+
+            // XamlXmlNamespaceManager
+
+            // var context = new XmlParserContext(null, xmlns, "", XmlSpace.Default);
+            // XamlSchemaContext xamlSchemaContext = new XamlSchemaContext();
+            // xamlSchemaContext.
+            // var xamlXmlReader = new XamlXmlReader(new StringReader(readText), xamlSchemaContext);
+            // xamlXmlReader.SchemaContext.GetAllXamlNamespaces()
+
+            
+            XamlXmlReaderSettings xamlXmlReaderSettings = new XamlXmlReaderSettings();
+            LogMessage l = new LogMessage();
+            xamlXmlReaderSettings.LocalAssembly = l.GetType().Assembly;
+
+            var stringReader = new StringReader(readText);
+            var xamlXmlReader = new XamlXmlReader(stringReader, xamlXmlReaderSettings);
+            var xamlReader = ActivityXamlServices.CreateBuilderReader(xamlXmlReader);
+
+            // UiPath.Core.Activities.
+
+            ActivityBuilder activityBuilder = XamlServices.Load(xamlReader) as ActivityBuilder;
+
+            if(activityBuilder == null)
+            {
+                return null;
+            }
+
+            Workflow workflow = new Workflow();
+
+            workflow.DisplayName = activityBuilder.Implementation.DisplayName;
+
+            // Arguments
+            foreach (var dynamicActivityProperty in activityBuilder.Properties)
+            {
+                workflow.Arguments.Add(dynamicActivityProperty);
+            }
+
+           // Structure
+           NativeActivity nativeActivity = activityBuilder.Implementation as NativeActivity;
+
+            if(nativeActivity == null)
+            {
+                return null;
+            }
+            INativeActivityParser parser = NativeActivityParserFactory.Instance.GetParser(nativeActivity);
+            parser.ParseNativeActivity(nativeActivity, workflow);
+
+            return workflow;
+        }
+
+    }
+}
