a66fd046f863de2e64903f2adb8b67759da1307e
paduraru2009 <ciprian.paduraru2009@gmail.com>
2021-05-09 11:58:01 +0300

    Multiple workflows collaborating together as a hypergraph ; Added entry node; Allowing now graphs to contain circles ; Improved graph visualization with colors and markers;

--- Fuzzer/SymbolicFuzzer/Main.py
+++ Fuzzer/SymbolicFuzzer/Main.py
@@ -55,350 +55,51 @@ from GraphDef import *
 # 3. solveOnlineDynamicSymbolic - concolic, with interaction nodes that requires feedback from robot
 # ----------
 
-class SymbolicWorflowTester():
-    def __init__(self, name : str, graphDict, graphVariables):
-        self.name = name # Name of the model
-        self.graph = None # Symbolic graph structure
-        self.V = {} # Variables dict
-        self.V_constants = {} # Context variables that are given as fixed for the model
-
-        # Create the variables dictionary inside self.V
-        self.__createVariables(graphVariables)
-
-        # Create the symbolic graph
-        self.graph = self.__createTestGraph_fromDict(graphDict, name)
-
-    # A factory of variables by name and specification!
-    def __createSingleVariableBySpec(self, varName, varSpec):
-        varType = varSpec
-        if not isinstance(varType, tuple):  # Simple types
-            if varType == 'Int':
-                self.V[varName] = z3.Int(varName)
-            elif varType == 'Bool':
-                self.V[varName] = z3.Bool(varName)
-            elif varType == 'Real':
-                self.V[varName] = z3.Real(varName)
-            elif varType == 'String':
-                self.V[varName] = z3.String(varName)
-
-            # TODO: there are two variants !!!
-            # elif varType == 'BitVec':
-            #    self.V[varName] == z3.BitVec()
-
-        else:  # Composed type
-            assert len(varType) > 1, f"Specification is incorrect for varName {varName} and varType {varType}"
-            varType = varSpec[0]
-            if varType == 'IntVector':
-                size = varSpec[1]
-                self.V[varName] = z3.IntVector(varName, size)
-            elif varType == 'BoolVector':
-                size = varSpec[1]
-                self.V[varName] = z3.BoolVector(varName, size)
-            elif varType == 'IntVector':
-                size = varSpec[1]
-                self.V[varName] = z3.RealVector(varName, size)
-            elif varType == 'Array':
-                indexSort = self.__fromStrSortToZ3Sort(varSpec[1])
-                valuesSort = self.__fromStrSortToZ3Sort(varSpec[2])
-                self.V[varName] = z3.Array(varName, indexSort, valuesSort)
-            elif varType == 'Function':
-                pass  # TODO fix later
-
-    # Parse a variables dictionary and stores them in this class
-    def __createVariables(self, graphVariables):
-        for varName, varSpec in graphVariables.items():
-            if isinstance(varSpec, tuple):
-                varType = varSpec[0]
-                if varType == 'Const': # This is special because:   (name , ('Const', Value, Sort)). Base type is (name, Sort)
-                    constVal = varSpec[1]
-                    self.V_constants[varName] = constVal
-                    varSpec = list(varSpec)[2:]
-                    if len(varSpec) == 1:
-                        varSpec = varSpec[0]
-                    else:
-                        varSpec = tuple(varSpec[0])
-
-            self.__createSingleVariableBySpec(varName, varSpec)
-
-            assert varName in self.V, f"Couldn't fix this variable name {varName} !!"
-
-
-    # Given a sort as a string convert to a real Z3 object sort
-    def __fromStrSortToZ3Sort(self, strSort : str):
-        if strSort == 'Int':
-            return z3.IntSort()
-        elif strSort == 'Bool':
-            return z3.BoolSort()
-        elif strSort == 'String':
-            return z3.StringSort()
-        elif strSort == 'Real':
-            return z3.RealSort()
-        elif strSort == 'Array':
-            return z3.ArraySort()
-        elif strSort == 'BitVec':
-            return z3.BitVecSort()
-        else:
-            assert f"Can't solve the given {strSort} type !"
-            return None
-        return None
-
-    # Parse a graph dictionary
-    def __createTestGraph_fromDict(self,dictSpec: Dict[str, any], graphName):
-        graph = nx.DiGraph()
-        graph.clear()
-
-        # Step 1: Create all the node firsts and cache the inverse dictionary in the graph from node_id to node instance
-        graph.graph['graphName'] = graphName
-
-        nodeIdToInstance: Dict[str, BranchNode] = {}
-
-        # Factory of nodes...too simple to put it now in a different class
-        for nodeId, nodeSpec in dictSpec.items():
-            assert isinstance(nodeSpec, tuple) and len(nodeSpec) > 0, f"invalid specification for node {nodeId}"
-            nodeCond = nodeSpec[0]
-
-            if nodeCond is None:
-                nodeInst = FlowNode(id=nodeId)
-            else:
-                nodeInst = BranchNode(id=nodeId, condition=nodeCond)
-
-            nodeIdToInstance[nodeId] = nodeInst
-            graph.add_node(nodeInst)
-
-        graph.graph['nodeIdToInstance'] = nodeIdToInstance
-
-        # Step 2: Create the links inside the nodes and graph
-        for nodeId, nodeSpec in dictSpec.items():
-            parentNodeInst = nodeIdToInstance[nodeId]
-
-            if parentNodeInst.nodeType == NodeTypes.BRANCH_NODE:
-
-                # Solve successors for each condition
-                nodeSuccessorsSpec = nodeSpec[1]
-                assert isinstance(nodeSuccessorsSpec, list), f"Expecting a list here for node successors desc!"
-                for nodeSucc in nodeSuccessorsSpec:
-                    nextNodeVal = nodeSucc[0]
-                    nextNodeId = nodeSucc[1]
-
-                    succNodeInst = nodeIdToInstance[nextNodeId]
-                    parentNodeInst.valuesAndNext[nextNodeVal] = nextNodeId
-
-                    graph.add_edge(parentNodeInst, succNodeInst)
-            elif parentNodeInst.nodeType == NodeTypes.FLOW_NODE:
-                assert nodeSpec[0] == None
-
-                # Solve successor
-                successor = nodeSpec[1]
-                assert successor is None or isinstance(successor, str)
-                parentNodeInst.nextNodeId = successor
-
-                if successor != None:
-                    succNodeInst = nodeIdToInstance[successor]
-                    graph.add_edge(parentNodeInst, succNodeInst)
-
-        return graph
-
-    # A function to collect all paths in the graph. For loops it just consider one iteration case as long as every branch is covered
-    # For infinite graphs we'll consider IDA approach
-    def __getAllPaths(self):
-        # Get all the starting nodes (in degree = 0)
-        start_nodes = []
-        for node in self.graph.nodes:
-            node = self.__fixNodeInstance(node)
-            if self.graph.in_degree[node] == 0:
-                start_nodes.append(node)
-
-        # print(start_nodes[0].name)
-
-        allpaths = []
-
-        def _findpath(graph, currNode, currPath, outAllPaths):
-            currPath.append(currNode)
-            if graph.out_degree[currNode] == 0:  # leaf node ?
-                outAllPaths.append(currPath)
-            else:
-                # For each successor add this node to a copy of the list and iterate on that path
-                for succNode in graph.successors(currNode):
-                    succNode = self.__fixNodeInstance(succNode)
-                    newCurrPath = copy.deepcopy(currPath)
-                    _findpath(graph, succNode, newCurrPath, outAllPaths)
-
-        # From each starting node, run a directed search path until leafs
-        for snode in start_nodes:
-            _findpath(self.graph, snode, [], allpaths)
-
-        return allpaths
-
-    # A function to fix the node finding inside a graph
-    def __fixNodeInstance(self, node):
-        if isinstance(node, str):
-            nodeIdToInstance = self.graph.graph['nodeIdToInstance']
-            node = nodeIdToInstance[node]
-        return node
-
-    def __debugPrintSinglePath(self, path):
-        pathStr = ""
-        for node in path:
-            node = self.__fixNodeInstance(node)
-            pathStr += str(node.id) + " ; "
-        return pathStr
-
-    def __debugPrintPaths(self, paths):
-        for index, P in enumerate(paths):
-            print("--- Path ", index, ": ")
-            pathStr = self.__debugPrintSinglePath(P)
-            print(pathStr)
-
-    def __debugInspectGraph(self):
-        print("Graph nodes: ")
-        for node in self.graph.nodes:
-            print(node)
-
-        print("Graph edges: ")
-        for edge in self.graph.edges:
-            start = edge[0]
-            end = edge[1]
-            print(f"start from {start.id} end {end.id}")
-
-        print("In Degrees: ", self.graph.in_degree([node for node in self.graph.nodes]))
-        print("Out Degrees: ", self.graph.out_degree([node for node in self.graph.nodes]))
-
-    def __getPathConditions(self, path):
-        assert isinstance(path, list) and len(path) > 0 and isinstance(path[0], BranchNode)
-        pathLen = len(path)
-        outCOnditions = []
-        for nodeIndex in range(pathLen):
-
-
-            currNode = path[nodeIndex]
-            if currNode.nodeType != NodeTypes.BRANCH_NODE:
-                continue
-
-            nextNode = path[nodeIndex + 1] if (nodeIndex + 1 < pathLen) and len(currNode.valuesAndNext) > 0 else None
-
-            # Fix the condition to solve
-            condToSolve = currNode.expression
-            if nextNode != None:
-                # Is inverse branch for next node ?
-                if 'False' in currNode.valuesAndNext and currNode.valuesAndNext['False'] == nextNode.id:
-                    condToSolve = 'Not(' + condToSolve + ')'
-                else:
-                    assert currNode.valuesAndNext['True'] == nextNode.id
-
-            outCOnditions.append(condToSolve)
-
-        return outCOnditions
-
-    # Debugging the graph...
-    def debugGraph(self, outputGraphFile=None):
-        print("Drawing and showing the graph")
-        #nx.draw_networkx(self.graph)
-        #plt.pyplot.show()
-        A = to_agraph(self.graph)
-        A.layout('dot')
-        A.draw(outputGraphFile)
-
-        # Let's inspect the graph...
-        self.__debugInspectGraph()
-
-        print("Checking all paths inside the graph !")
-        allpaths = self.__getAllPaths()
-        self.__debugPrintPaths(allpaths)
-
-        print("\n\nGetting all used variables inside branches ")
-        print(self.V)  # print(getAllVariables(graph))
-
-    # Solve all feasible paths inside the graph and produce optionally a csv output inside a given csv file
-    def solveOfflineStaticGraph(self, outputCsvFile=None, debugLogging=False):
-        V = self.V
-
-        fieldNamesList = [key for key in self.V_constants] # Put the constants firsts
-        fieldNamesList.extend([key for key in self.V if key not in self.V_constants])
-        set_fieldNamesList = set(fieldNamesList)
-
-        csv_stream = None
-        if outputCsvFile != None:
-            csv_file = open(outputCsvFile, mode='w')
-            csv_stream = csv.DictWriter(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldNamesList)
-            csv_stream.writeheader()
-
-        #condA = 'V["loan"] < 1000' # Just a dummy test to evaluate a simple condition...
-        #eval(condA)
-        allpaths = self.__getAllPaths()
-
-        for index, P in enumerate(allpaths):
-            if debugLogging:
-                print(f"Analyzing path {index}: [{self.__debugPrintSinglePath(P)}]")
-                print("-------------------------")
-            conditions_str = self.__getPathConditions(allpaths[index])
-
-            conditions_z3 = []
-            for s in conditions_str:
-                cond = eval(s)
-                conditions_z3.append(cond)
-
-            solver = Solver()
-            # Add all the required conditions for the constant variables
-            for constName, constValue in self.V_constants.items():
-                solver.add(self.V[constName] == constValue)
-
-            # Add all the required conditions for the path
-            for cond_z3 in conditions_z3:
-                solver.add(cond_z3)
-
-            isSolution = solver.check()
-            # print(isSolution)
-            if isSolution and isSolution != z3.unsat:
-                if debugLogging:
-                    print("Found a solution")
-                m = solver.model()
-
-                # Print debug all declarations
-                for d in m.decls():
-                    if debugLogging:
-                        print(f"{d.name()}={m[d]}")
-
-                # Get one output row for csv extract
-                if csv_stream != None:
-                    rowContent = {}
-                    for decl in m.decls():
-                        declAsString = str(decl)
-                        if declAsString in set_fieldNamesList:
-                            valueOfDecl = m[decl]
-                            if z3.is_int_value(valueOfDecl):
-                                valueOfDecl = valueOfDecl.as_long()
-                            elif z3.is_real(valueOfDecl):
-                                valueOfDecl = float(valueOfDecl.as_decimal(prec=2))
-
-                            elif z3.is_bool(valueOfDecl):
-                                valueOfDecl = True if z3.is_true(valueOfDecl) else False
-
-                            rowContent[declAsString] = valueOfDecl
-
-                    if csv_stream:
-                        csv_stream.writerow(rowContent)
-            else:
-                if debugLogging:
-                    print("No solution exists for this path")
-
 def demo_bankLoanExample():
-    variables = {'loan' : 'Real', 'term' : 'Int', 'context_userId' : ('Const', 2, 'Int')}
 
-    bankLoanGraph = {'node_loanTest0' : ('V["loan"] < 1000', [('True', 'lowVolumeLoan'), ('False', 'node_loanTest1')]),
-                     'lowVolumeLoan' : (None, 'term_test0'),
-                    'node_loanTest1' : ('And(V["loan"] >= 1000, V["loan"] < 100000)', [('True', 'midVolumeLoan'), ('False', 'highVolumeLoan')]),
-                    'lowVolumeLoan' : (None, 'term_test0'),
-                     'midVolumeLoan' : (None, 'term_test0'),
-                    'highVolumeLoan' : (None, 'term_test0'),
-                    'term_test0' : ('V["term"] < 5', [('True', 'shortTermLoan'), ('False', 'longTermLoan')]),
-                    'shortTermLoan' :  (None, 'outputRate'),
-                    'longTermLoan': (None, 'outputRate'),
-                     'outputRate' : (None, None)
+    # Workflow 1: the bank loan main (embedding a PIN test grpah inside)
+    # Check the rules for embedding as written below in comments
+    variables_bankLoanMainGraph = {'Main:loan' : 'Real', 'Main:term' : 'Int', 'Main:context_userId' : ('Context', 2, 'Int')}
+
+    bankLoanMainGraph = {'Main:node_loanTest0' : ('V["Main:loan"] < 1000', [('True', 'Main:lowVolumeLoan'), ('False', 'Main:node_loanTest1')]),
+                     'Main:lowVolumeLoan' : (None, 'Main:term_test0'),
+                    'Main:node_loanTest1' : ('And(V["Main:loan"] >= 1000, V["Main:loan"] < 100000)', [('True', 'Main:midVolumeLoan'), ('False', 'Main:highVolumeLoan')]),
+                    'Main:lowVolumeLoan' : (None, 'Main:term_test0'),
+                     'Main:midVolumeLoan' : (None, 'Main:term_test0'),
+                    'Main:highVolumeLoan' : (None, 'Pin:checkPin'),
+
+                    'Pin:FailedCheck' : (None, 'Main:fail'), # These 2 nodes will not be created inside this graph ! Only the links will be created
+                    'Pin:SucceedCheck' : (None, 'Main:term_test0'),
+
+                    'Main:term_test0' : ('V["Main:term"] < 5', [('True', 'Main:shortTermLoan'), ('False', 'Main:longTermLoan')]),
+                    'Main:shortTermLoan' :  (None, 'Main:outputRate'),
+                    'Main:longTermLoan': (None, 'Main:outputRate'),
+                     'Main:outputRate' : (None, None),
+                    'Main:fail' : (None, 'Main:node_loanTest0')
                     }
 
-    bankLoanWorkTester = SymbolicWorflowTester(name="bankLoanWorkflow", graphDict=bankLoanGraph, graphVariables=variables)
-    bankLoanWorkTester.debugGraph(outputGraphFile="debugGraph.png")
-    bankLoanWorkTester.solveOfflineStaticGraph(outputCsvFile="generatedTests.csv", debugLogging=False)
+    bankLoanWorkflow = WorkflowDef(variables=variables_bankLoanMainGraph, graph=bankLoanMainGraph, name="Main", color='red')
+
+
+    # The second workflow definition
+    variables_bankPinTest = {'Pin:local_pinCorrect' : ('Context', False, 'Bool'), 'Pin:local_numberRetries' : ('Context', 0, 'Int')}
+    # Notes:    - checkPin node should write local_piCorrect to either true or false and increase the local_nu
+    #           - retryFailed should increment the local_numberRetries
+    bankPinTest = { 'Pin:checkPin' : (None, 'Pin:pinTest0'),
+                    'Pin:pinTest0' : ('V["Pin:local_pinCorrect"] == True', [('True', 'Pin:SucceedCheck'), ('False', 'Pin:retryFailed')]),
+                     'Pin:retryFailed' : (None, 'Pin:checkRetryTest'),
+                    'Pin:checkRetryTest' : ('V["Pin:local_numberRetries"] < 3', [('True', 'Pin:checkPin'), ('False', 'Pin:FailedCheck')]),
+                     'Pin:SucceedCheck' : (None, None),
+                     'Pin:FailedCheck' : (None, None),
+                     }
+
+    bankPinWorkflow = WorkflowDef(variables=variables_bankPinTest, graph=bankPinTest, name = "Pin", color='blue')
+
+    allWorkflows : List[WorkflowDef] = [bankPinWorkflow, bankLoanWorkflow]
+    testFuzzer = SymbolicWorflowsTester(workflows=allWorkflows, mainWorflowName=bankLoanWorkflow.name, entryTestNodeId='Main:node_loanTest0')
+    testFuzzer.debugGraph(outputGraphFile="debugGraph.png")
+    testFuzzer.solveOfflineStaticGraph(outputCsvFile="generatedTests.csv", debugLogging=False)
+
 
 if __name__ == "__main__":
     # Creating the graph now from a given dictionary
