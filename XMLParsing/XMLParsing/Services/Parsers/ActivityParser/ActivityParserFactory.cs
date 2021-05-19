7967fd4f70b94a945d4f114c65b1fe9a392a8924
Vlad Coteanu <vlad.coteanu@uipath.com>
2021-05-19 22:54:07 +0300

    Added sequence parsing. Added Uipath.Testing.Activities dependency.

---
+++ XMLParsing/XMLParsing/Services/Parsers/ActivityParser/ActivityParserFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using XMLParsing.Services.Parsers.ActivityParser;
+using XMLParsing.Services.Parsers.NativeActivityParser;
+
+namespace XMLParsing.Services
+{
+    class ActivityParserFactory
+    {
+        private static readonly Lazy<ActivityParserFactory> LazyInstance = new Lazy<ActivityParserFactory>(() => new ActivityParserFactory());
+
+        private ActivityParserFactory()
+        {
+        }
+
+        public static ActivityParserFactory Instance
+        {
+            get
+            {
+                return LazyInstance.Value;
+            }
+        }
+
+        public IActivityParser GetParser(Activity activity)
+        {
+            if(activity == null)
+            {
+                return null;
+            }
+
+            var activityParserDict = new Dictionary<Type, Func<IActivityParser>>
+            {
+                { typeof(Flowchart), () => new FlowchartParser() },
+                { typeof(Sequence), () => new SequenceParser() }
+            };
+
+            if (activityParserDict.ContainsKey(activity.GetType()))
+            {
+                return activityParserDict[activity.GetType()]();
+            }
+            else
+            {
+                return new DefaultActivityParser();
+            }
+        }
+    }
+
+}
---
+++ XMLParsing/XMLParsing/Services/Parsers/ActivityParser/DefaultActivityParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Activities;
+using XMLParsing.Common;
+using XMLParsing.Utils;
+
+namespace XMLParsing.Services.Parsers.ActivityParser
+{
+    class DefaultActivityParser : IActivityParser
+    {
+        public Tuple<Node, Node> ParseActivity(Activity activity, Workflow workflow)
+        {
+            Node node = ActivityUtils.CreateSimpleNodeFromActivity(activity);
+            workflow.Nodes.Add(node);
+            return Tuple.Create(node, node);
+        }
+    }
+}
