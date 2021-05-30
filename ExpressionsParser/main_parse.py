337601fb04bc505825a91b4d64061a5120f8f85e
Ana Turlea <turleaana@gmail.com>
2021-05-30 23:15:08 +0300

    added color and name to expression parser output

--- ExpressionsParser/main_parse.py
+++ ExpressionsParser/main_parse.py
@@ -1,18 +1,20 @@
-
 import myparser
 import graphviz
 import json
 import sys
+import pickle
 
 DataTypes = {
-    'Int32':'Int',
+    'Int32': 'Int',
     'String': 'String',
     'Boolean': 'Bool',
-    "Double":'Real',
-    "Decimal":'Decimal'
-    }
+    "Double": 'Real',
+    "Decimal": 'Decimal'
+}
 
- 
+Colors = ["red", "blue", "yellow", "green", "violet", "orange"]
+
+
 def parseGraph(path, out_path):
     with open(path) as json_file:
         data = json.load(json_file)
@@ -22,46 +24,55 @@ def parseGraph(path, out_path):
         z3Vars = {}
         z3Graph = {}
         for v in variables:
-            #z3Vars[v] = DataTypes[variables[v]]
-            z3Vars[rname+':'+v] = DataTypes[variables[v]]
-            #z3Vars['Main:'+v] = DataTypes[variables[v]]
+            # z3Vars[v] = DataTypes[variables[v]]
+            z3Vars[rname + ':' + v] = DataTypes[variables[v]]
+            # z3Vars['Main:'+v] = DataTypes[variables[v]]
         graph = data['graph']
         for k in graph:
-           
-            #print(graph[k])
+
+            # print(graph[k])
             # k is the node name 
             # graph[k] contains all info from a node
             # name = rname+':'+ k
             name = k
-            #the guard will contain the expression parsed using myparser
+            # the guard will contain the expression parsed using myparser
             if graph[k]['expression'] == '':
                 guard = 'None'
             else:
                 ast = myparser.parse(graph[k]['expression'])
-                guard = ast_to_string(ast,rname)
+                guard = ast_to_string(ast, rname)
             # transitions contain a list of transitions 
-            transitions=[]
+            transitions = []
             for trans in graph[k]['transitions']:
-                transition = (trans['value'],trans['destination'])
-                #transition = (trans['value'],rname+':'+trans['destination'])
+                transition = (trans['value'], trans['destination'])
+                # transition = (trans['value'],rname+':'+trans['destination'])
                 transitions.append(transition)
             if len(transitions) == 0:
-                 z3Graph[name]="(\""+ str(guard) + "\"," + 'None' +")"
+                z3Graph[name] = "(\"" + str(guard) + "\"," + 'None' + ")"
             elif len(transitions) == 1:
-                 z3Graph[name]="(\"" + str(guard) + "\","+ str(transitions[0]) +")"
-                  
+                z3Graph[name] = "(\"" + str(guard) + "\"," + str(transitions[0]) + ")"
+
             elif len(transitions) == 2:
-                 z3Graph[name]= "(\"" + str(guard) + "\"," + "[" + str(transitions[0]) + "," +str(transitions[1]) +"]" +")"
-                 
+                z3Graph[name] = "(\"" + str(guard) + "\"," + "[" + str(transitions[0]) + "," + str(
+                    transitions[1]) + "]" + ")"
+
         print(z3Vars)
         print(z3Graph)
         z3GraphStr = ""
-        for (k,v) in z3Graph.items():
-            z3GraphStr = z3GraphStr + "\"" +k +"\"" + ": " + v +"\n" 
-        data = "{" + "\"variables\"" + ": " +  str(z3Vars) +",\n" + 'graph'+": { \n"+ z3GraphStr + "}\n }"
+        for (k, v) in z3Graph.items():
+            z3GraphStr = z3GraphStr + "\"" + k + "\"" + ": " + v + "\n"
+        data = "{" + "\"variables\"" + ": " + str(z3Vars) + ",\n" + \
+               "\"graph\": { \n" + z3GraphStr + "},\n " + \
+               '\"debugColor\": \"' + Colors[(hash(rname) % len(Colors))] + "\",\n " + \
+               "\"name\": \"" + rname + " \" }"
         text_file = open(out_path, "w")
         text_file.write(data)
         text_file.close()
+        # dataToSave = {"variables": z3Vars,"graph": z3GraphStr, "debugColor": Colors[(hash(rname) % len(Colors))], "name": rname}
+        #
+        # f = open('out_path.pkl', 'wb')  # Pickle file is newly created where foo1.py is
+        # pickle.dump(dataToSave, f)  # dump data to f
+        # f.close()
         '''
         with open(out_path, 'w') as outfile:
              json.dump(data, outfile,indent=4)
@@ -74,50 +85,49 @@ def parseGraph(path, out_path):
         # return z3Graph
 
 
-def ast_to_string(localast,rname):
+def ast_to_string(localast, rname):
     # AST to string - using preorder 
     # to change for ternary operations
     if localast.token_type == myparser.TokenType.T_VAR:
         # localstr = 'V['+ localast.value+']'
-        localstr = 'V['+rname+':'+ localast.value+']'
+        localstr = 'V[' + rname + ':' + localast.value + ']'
     else:
         localstr = localast.value
     st = ''
     dr = ''
-    if len(localast.children) > 0 :
-        
+    if len(localast.children) > 0:
+
         if len(localast.children[0].children) == 0:
-           st = str(ast_to_string(localast.children[0],rname))
+            st = str(ast_to_string(localast.children[0], rname))
         else:
-           st =   "(" + str(ast_to_string(localast.children[0],rname))+")"
-        if len(localast.children) ==2:
+            st = "(" + str(ast_to_string(localast.children[0], rname)) + ")"
+        if len(localast.children) == 2:
             if len(localast.children[1].children) == 0:
-              dr = str(ast_to_string(localast.children[1],rname))
+                dr = str(ast_to_string(localast.children[1], rname))
             else:
-              dr =  "(" + str(ast_to_string(localast.children[1],rname))+")"
-    
+                dr = "(" + str(ast_to_string(localast.children[1], rname)) + ")"
+
     if st == '' and dr == '':
-        localstr = localstr 
+        localstr = localstr
     elif st == '' and dr != '':
-        localstr = localstr + ' ' + dr 
+        localstr = localstr + ' ' + dr
     elif st != '' and dr == '':
         localstr = localstr + ' ' + st
-    elif localast.value in ["And",'Or']:
+    elif localast.value in ["And", 'Or']:
         localstr = localstr + '(' + st + ',' + dr + ')'
     else:
         localstr = st + ' ' + localstr + ' ' + dr
     return localstr
 
+
 def test_parser(inputstring):
     ast = myparser.parse(inputstring)
     graphviz.label(ast)
     graphviz.to_graphviz(ast)
- 
-    
- 
+
 
 if __name__ == '__main__':
-    path = '..\\Models\\' + sys.argv[1] # "SimpleBankLoan\Pin Check_202105121449166565.json"
+    path = '..\\Models\\' + sys.argv[1]  # "SimpleBankLoan\Pin Check_202105121449166565.json"
     # rname = sys.argv[2] # Main
     # out_path = path[:-5] + '_parsed.json'
     x = path.rfind("\\")
@@ -141,10 +151,8 @@ graph
 
 
 '''
-#test_parser('(1+7)*(9+2)')
-#test_parser('(6 < a)' )
+# test_parser('(1+7)*(9+2)')
+# test_parser('(6 < a)' )
 
-#test_parser('term < 5' )
-#test_parser('(loan >= 1000) and (loan < 100000)' )
-
-
+# test_parser('term < 5' )
+# test_parser('(loan >= 1000) and (loan < 100000)' )
