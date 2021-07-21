55b188023906db347745105f171003c1bb097962
Vlad Coteanu <vlad.coteanu@uipath.com>
2021-07-21 14:44:57 +0300

    Create C# version of Main. Improve ReadCSV consistency.

--- XMLParsing/XMLParsing/Common/NodeExtensions/ReadCsvFileNode.cs
+++ XMLParsing/XMLParsing/Common/NodeExtensions/ReadCsvFileNode.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace XMLParsing.Common.NodeExtensions
 {
@@ -26,8 +22,7 @@ namespace XMLParsing.Common.NodeExtensions
         public override void AddAdditionalNodeInformation(IDictionary<string, object> nodeInformation)
         {
             IDictionary<string, string> readCsvInfo = new Dictionary<string, string>();
-            readCsvInfo.Add("expression", DataTable + " = " + "LoadCSV(" + CsvFilePath + ")");
-            nodeInformation.Add("additional_info", readCsvInfo);
+            nodeInformation.Add("expression", DataTable + " = " + "LoadCSV(" + CsvFilePath + ")");
         }
     }
 }
